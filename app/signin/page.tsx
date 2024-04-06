35e91acf12b47432a8e9ce98028b4de2e1e98ca3
Marwan <kaka.mer1998@gmail.com>
2024-04-06 06:09:09 +0300

    feat: create middleware for redirects after login && finish form handing

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,12 +1,23 @@
-'use client'
+"use client";
 import React from "react";
 import { Divider, Input, Button } from "@nextui-org/react";
 import { title, subtitle } from "@/components/primitives";
 import { EyeFilledIcon, EyeSlashFilledIcon } from "@/components/icons";
-
+import { useRouter } from "next/navigation";
 function Page() {
   const [isVisible, setIsVisible] = React.useState(false);
   const toggleVisibility = () => setIsVisible(!isVisible);
+  const router = useRouter()
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  function handelSubmit(event:any){
+    event.preventDefault();
+    localStorage.setItem('username',username)
+    console.log(username);
+    router.push('/')
+    
+  }
 
   return (
     <div className="flex items-center justify-center text-center">
@@ -25,35 +36,47 @@ function Page() {
         </div>
 
         <div className="p-10">
-          <Input
-            variant="bordered"
-            type="email"
-            labelPlacement="inside"
-            label=" اسم المستخدم "
-          />
-          <div className="my-4">
+          <form onSubmit={handelSubmit}>
             <Input
-              label="كلمة السر"
               variant="bordered"
-              placeholder=""
-              endContent={
-                <button
-                  className="focus:outline-none"
-                  type="button"
-                  onClick={toggleVisibility}
-                >
-                  {isVisible ? (
-                    <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                  ) : (
-                    <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                  )}
-                </button>
-              }
-              type={isVisible ? "text" : "password"}
-              className="max-w-xs"
+              type="email"
+              labelPlacement="inside"
+              label=" اسم المستخدم "
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
             />
-          </div>
-          <Button color="primary">تسجيل الدخول </Button>
+            <div className="my-4">
+              <Input
+                label="كلمة السر"
+                variant="bordered"
+                placeholder=""
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                endContent={
+                  <button
+                    className="focus:outline-none"
+                    type="button"
+                    onClick={toggleVisibility}
+                  >
+                    {isVisible ? (
+                      <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                    ) : (
+                      <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                    )}
+                  </button>
+                }
+                type={isVisible ? "text" : "password"}
+                className="max-w-xs"
+              />
+            </div>
+            <Button onClick={handelSubmit} color="primary">
+              تسجيل الدخول{" "}
+            </Button>
+          </form>
         </div>
       </div>
     </div>
