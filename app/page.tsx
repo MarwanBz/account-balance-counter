bfa9550edecd33a16ccd0b9a0549d17484da8c33
Marwan <kaka.mer1998@gmail.com>
2024-04-06 03:01:03 +0300

    feat(home): add redirect func to the signin page

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,10 @@ import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 import { Counter } from "@/components/counter";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  redirect('signin')
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-lg text-center justify-center">
